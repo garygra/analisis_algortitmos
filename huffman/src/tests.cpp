ba0aade82efdca0422dad9a39efca80caf2d4b11
Edgar Granados Osegueda <edgar.granados@itam.mx>
2018-11-21 10:50:50 -0600

    finished implementation

--- huffman/src/tests.cpp
+++ huffman/src/tests.cpp
@@ -6,7 +6,7 @@
 #include <sstream>
 #include <random>
 #include <math.h> 
-
+#include <ctime>
 // #include "node.hpp"
 // #include "priority_queue.hpp"
 #include "huffman.hpp"
@@ -25,9 +25,22 @@ int main (int argc, char *argv[])
 
 	huffman huffman;
 	
-	vector<char> v_chars{ 'a', 'b', 'c', 'd', 'e', 'f' }; 
-	vector<int>    v_freqs{ 45, 13, 12, 16, 9, 5 }; 
-	
+	// vector<char> v_chars{ 'a', 'b', 'c', 'd', 'e', 'f' }; 
+	// vector<int>    v_freqs{ 45000, 13000, 12000, 16000, 9000, 5000 }; 
+	vector<char> v_chars; 
+	vector<int>  v_freqs; 
+	char letter = 'a';
+	int freq = 0;
+	srand(time(NULL));
+	for (int i = 0; i < 26; ++i)
+	{
+		v_chars.push_back(letter);
+		freq = (rand() % 100) * 1000;
+		v_freqs.push_back(freq);
+		cout << letter << ":" << freq << ". ";
+		letter++;
+	}
+	cout << endl;
 
 	huffman.initialize(v_chars, v_freqs);
 
